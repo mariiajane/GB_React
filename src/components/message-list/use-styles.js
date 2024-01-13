1b3a1e6e04c3db5deb6c33ceed0a68632d32815f
Mariia Pushkareva <m.pushkareva@intetics.com>
2024-01-13 17:06:39 +0100

    lesson3

---
+++ src/components/message-list/use-styles.js
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const useStyles = styled(() => {
+    return{
+        input: {
+            color: "#9a9fa1",
+            padding: "15px 15px",
+            fontSize:"15px",
+        },
+        icon: {
+            color: "#2b5278",
+            cursor:"pointer",
+        }
+    };
+});
--- src/index.js
+++ src/index.js
@@ -1,73 +1,22 @@
-import React, { useState, useEffect} from 'react';
+import React  from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { render } from '@testing-library/react';
-import { renderIntoDocument } from 'react-dom/test-utils';
+import {MessageList, Layout, ChatList, Header } from './components';
+//import "./global.css";
 
-const FunctionComponent = ({ onClick }) => {
-
-  const [messageList, setMessageList] = useState([])
-  const [inputText, setInputText] = useState('')
-
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-  };
-
-  const handleSendMessage = () => {
-    if (inputText.trim() !== '') {
-      const newMessage = {
-        text: inputText,
-        author: 'user',
-      };
-
-      setMessageList([...messageList, newMessage]);
-      setInputText('');
-    }
-  };
-  useEffect(() => {
-    const lastMessage = messageList.length > 0 ? messageList[messageList.length - 1] : null;
-
-    if (lastMessage && lastMessage.author === 'user') {
-      // Добавляем задержку перед ответом робота
-      const robotResponseTimeout = setTimeout(() => {
-        const robotMessage = {
-          text: 'Спасибо за ваше сообщение. Я робот созданный учеником GB.',
-          author: 'robot',
-        };
-        setMessageList([...messageList, robotMessage]);
-      }, 1500);
-
-      // Очищаем таймаут при размонтировании компонента или при следующем сообщении пользователя
-      return () => clearTimeout(robotResponseTimeout);
-    }
-  }, [messageList]);
-
-  return (
-    <div>
-      <div>
-        {messageList.map((message, index) => (
-          <div key={index} className={message.author === 'robot' ? 'robot-message' : 'user-message'}>
-            <strong>{message.author}: </strong>{message.text}
-          </div>
-        ))}
-      </div>
-      <div>
-        <input type="text" value={inputText} onChange={handleInputChange} />
-        <button onClick={handleSendMessage}>ОТПРАВИТЬ</button>
-      </div>
-    </div>
-  );
-};
-const App = () => {
+const Appl = () => {
   return(
     <>
-    {<FunctionComponent/>}
+    <Layout 
+    messages={<MessageList/>} 
+    chats = {<ChatList/>}
+    header = {<Header/>}
+    />
     </>
   )
 }
 ReactDOM.render(
    <React.StrictMode>
-   <App/>
+   <Appl/>
    </React.StrictMode>,
  document.getElementById('root')
  );
